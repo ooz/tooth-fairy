10a257b5f4f34923117e925776f6c56b83438af3
ooz <ooz@users.noreply.github.com>
2023-02-03 23:26:15 +0100

    Layout teeth

--- tooth-fairy.js
+++ tooth-fairy.js
@@ -33,29 +33,42 @@ function preload() {
     loadAsset(this, "croc-face-neutral-transparent");
 
     loadAsset(this, "tooth");
+
+    loadAsset(this, "tooth-01-transparent");
+    loadAsset(this, "tooth-02-transparent");
+    loadAsset(this, "tooth-03-transparent");
+    loadAsset(this, "tooth-04-transparent");
+    loadAsset(this, "tooth-05-transparent");
+    loadAsset(this, "tooth-06-transparent");
+    loadAsset(this, "tooth-08-transparent");
+    loadAsset(this, "tooth-09-transparent");
+    loadAsset(this, "tooth-10-transparent");
+    loadAsset(this, "tooth-11-transparent");
+    loadAsset(this, "tooth-12-transparent");
 }
 
 function create() {
     this.add.image(150, 300, "croc-mouth");
     this.add.image(150, 300, "croc-face-neutral-transparent");
 
-    _gameState.teeth.push(new Tooth(this, 30, 300));
-    _gameState.teeth.push(new Tooth(this, 30, 400));
-    _gameState.teeth.push(new Tooth(this, 30, 500));
+    _gameState.teeth.push(new Tooth(this, 10, 260, "tooth-01-transparent"));
+    _gameState.teeth.push(new Tooth(this, 10, 360, "tooth-02-transparent"));
+    _gameState.teeth.push(new Tooth(this, 10, 470, "tooth-03-transparent"));
+
+    _gameState.teeth.push(new Tooth(this, 30, 600, "tooth-04-transparent"));
+    _gameState.teeth.push(new Tooth(this, 120, 600, "tooth-05-transparent"));
+    _gameState.teeth.push(new Tooth(this, 210, 600, "tooth-06-transparent"));
+    _gameState.teeth.push(new Tooth(this, 280, 580, "tooth-08-transparent"));
+
+    _gameState.teeth.push(new Tooth(this, 290, 260, "tooth-09-transparent"));
+    _gameState.teeth.push(new Tooth(this, 290, 350, "tooth-10-transparent"));
+    _gameState.teeth.push(new Tooth(this, 290, 430, "tooth-11-transparent"));
+    _gameState.teeth.push(new Tooth(this, 290, 500, "tooth-12-transparent"));
 }
 
 function update(t, dt) {
     for (let tooth of _gameState.teeth) {
         //tooth.bounceBack();
-    }
-}
-
-function onDown() {
-    // Fullscreen
-    game.paused = false;
-    if (!game.scale.isFullScreen) {
-        game.scale.startFullScreen(false);
-        return;
     }
 }
 
